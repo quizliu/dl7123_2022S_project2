01ca65def885fb9d12c4666286da68de3d02a544
quizliu <51880883+quizliu@users.noreply.github.com>
2022-05-17 02:52:15 -0400

    fix convnext downsample upsample size

--- tasks/semantic/modules/convnext_model.py
+++ tasks/semantic/modules/convnext_model.py
@@ -80,14 +80,14 @@ class ConvNeXt(nn.Module):
 		self.dims = dims
 		self.downsample_layers = nn.ModuleList()  # stem and 3 intermediate downsampling conv layers
 		stem = nn.Sequential(
-			nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+			nn.Conv2d(in_chans, dims[0], kernel_size=3, stride=[1, 4], padding=[1, 0]),
 			LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
 		)
 		self.downsample_layers.append(stem)
 		for i in range(3):
 			downsample_layer = nn.Sequential(
 				LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
-				nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2),
+				nn.Conv2d(dims[i], dims[i + 1], kernel_size=3, stride=[1, 2], padding=[1, 1]),
 			)
 			self.downsample_layers.append(downsample_layer)
 
@@ -117,14 +117,14 @@ class ConvNeXt(nn.Module):
 			)
 			self.unconv = nn.ModuleList()  # unconv layers, [3] -> [2] -> [1] -> [0]
 			self.unconv.append(nn.Sequential(
-				nn.ConvTranspose2d(self.dims[0], 32, kernel_size=4, stride=4, padding=0),
+				nn.ConvTranspose2d(self.dims[0], 32, kernel_size=[3, 4], stride=[1, 4], padding=[1, 0]),
 				LayerNorm(32, eps=1e-6, data_format="channels_first"),
 				nn.GELU(),
 				Block(dim=32),
 			))
 			for i in range(3):
 				self.unconv.append(nn.Sequential(
-					nn.ConvTranspose2d(self.dims[i + 1], self.dims[i], kernel_size=2, stride=2, padding=0),
+					nn.ConvTranspose2d(self.dims[i + 1], self.dims[i], kernel_size=[3, 2], stride=[1, 2], padding=[1, 0]),
 					LayerNorm(self.dims[i], eps=1e-6, data_format="channels_first"),
 					nn.GELU(),
 					Block(self.dims[i])
